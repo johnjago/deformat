bca6bdef01550b5b3c8e35d77ca359720266d76d
johnjago <johnjago@users.noreply.github.com>
2016-07-10 18:59:34 -0500

    Add spaces

--- deformat.js
+++ deformat.js
@@ -1,7 +1,7 @@
 function deformat(input) {
 	var text = document.getElementById('input').value;
 	var strippedText = text.replace(/<(?:.|\n)*?>/gm, '');
-	var strippedText = text.replace(/(\r\n|\n|\r)/gm," ");
-	var strippedText = text.replace(/\s+/g," ");
+	var strippedText = text.replace(/(\r\n|\n|\r)/gm, " ");
+	var strippedText = text.replace(/\s+/g, " ");
 	document.getElementById('output').value = strippedText;
 }
