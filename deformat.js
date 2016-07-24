bca4a81f780b876474a4fec5f05f34387a3e390f
johnjago <johnjago@users.noreply.github.com>
2016-07-24 15:32:04 -0500

    Add function to turn off auto select

--- deformat.js
+++ deformat.js
@@ -1,7 +1,13 @@
 function deformat(input) {
 	var text = document.getElementById('input').value;
+	
 	var strippedText = text.replace(/<(?:.|\n)*?>/gm, '');
 	var strippedText = text.replace(/(\r\n|\n|\r)/gm, " ");
 	var strippedText = text.replace(/\s+/g, " ");
+	
 	document.getElementById('output').value = strippedText;
+}
+
+function turnOffAutoSelect() {
+	var output = document.getElementById('output').onclick = '';
 }
