f306ca77463a25780b1614caf2e6ec662edc2521
johnjago <johnjago@users.noreply.github.com>
2017-01-26 22:41:37 -0600

    Remove minified files

--- js/min.js
+++
@@ -1,1 +0,0 @@
-function deformat(input){var text=document.getElementById('input').value;var strippedText=text.replace(/<(?:.|\n)*?>/gm,'');var strippedText=text.replace(/(\r\n|\n|\r)/gm," ");var strippedText=text.replace(/\s+/g," ");document.getElementById('output').value=strippedText}function turnOffAutoSelect(){var output=document.getElementById('output').onclick=''}
