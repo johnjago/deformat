3c35b4cc9a6685020fca1e36821fb82350c32d47
John Jago <johnjago@protonmail.com>
2017-08-13 17:51:11 -0500

    Reduce code

--- src/deformat.js
+++ src/deformat.js
@@ -1,11 +1,9 @@
-// @license magnet:?xt=urn:btih:e95b018ef3580986a04669f1b5879592219e2a7a&dn=public-domain.txt Unlicense
+// magnet:?xt=urn:btih:1f739d935676111cfff4b4693e3816e664797050&dn=gpl-3.0.txt GNU GPLv3
 document.getElementById("deformat").addEventListener("click", function() {
-  var text = document.getElementById('input').value;
-  text = text.replace(/(\r\n|\n|\r)/gm, " ").replace(/\s+/g, " ");
-  document.getElementById("input").value = text;
-}, false);
+  document.getElementById("text").value = document.getElementById("text").value.replace(/(\r\n|\n|\r)/gm, " ").replace(/\s+/g, " ");
+});
 
 document.getElementById("clear").addEventListener("click", function() {
-  document.getElementById("input").value = "";
-}, false);
+  document.getElementById("text").value = "";
+});
 // @license-end
