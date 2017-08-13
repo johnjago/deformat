1935f5bda3250b48faca047be6e11b9b525ccafd
John Jago <johnjago@protonmail.com>
2017-08-13 17:51:22 -0500

    Add information

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var uglify = require('gulp-uglify');
 var cleanCSS = require('gulp-clean-css');
 var htmlmin = require('gulp-htmlmin');
 
-gulp.task('minify-js', function() {
+gulp.task('js', function() {
   return gulp.src('src/*.js')
     .pipe(uglify({ output: {
       comments: true
@@ -11,16 +11,16 @@ gulp.task('minify-js', function() {
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('minify-css', function() {
+gulp.task('css', function() {
   return gulp.src('src/*.css')
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('minify-html', function() {
+gulp.task('html', function() {
   return gulp.src('src/*.html')
     .pipe(htmlmin({collapseWhitespace: true}))
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', ['minify-js', 'minify-css', 'minify-html']);
+gulp.task('default', ['js', 'css', 'html']);
