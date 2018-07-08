c9080ccb4efa40a3893e7fa96dfddd909463601c
John Jago <johnjago@protonmail.com>
2018-07-08 15:07:42 -0500

    Remove magnet link and break up long line

--- src/deformat.js
+++ src/deformat.js
@@ -1,9 +1,8 @@
-// magnet:?xt=urn:btih:1f739d935676111cfff4b4693e3816e664797050&dn=gpl-3.0.txt GNU GPLv3
 document.getElementById("deformat").addEventListener("click", function() {
-  document.getElementById("text").value = document.getElementById("text").value.replace(/(\r\n|\n|\r)/gm, " ").replace(/\s+/g, " ");
+  var text = document.getElementById("text");
+  text.value = text.value.replace(/(\r\n|\n|\r)/gm, " ").replace(/\s+/g, " ");
 });
 
 document.getElementById("clear").addEventListener("click", function() {
   document.getElementById("text").value = "";
 });
-// @license-end
