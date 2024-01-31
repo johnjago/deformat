9f9a7206041bbe5a30a828856acad4d1baad24b9
John Jago <jc@johnjago.com>
2024-01-31 16:11:57 -0600

    Add a copy button

--- src/deformat.js
+++ src/deformat.js
@@ -6,3 +6,18 @@ document.getElementById("deformat").addEventListener("click", function() {
 document.getElementById("clear").addEventListener("click", function() {
 	document.getElementById("text").value = "";
 });
+
+document.getElementById("copy").addEventListener("click", function () {
+	const text = document.querySelector("#text");
+	text.select();
+	document.execCommand("copy");
+	showToast();
+});
+
+function showToast() {
+	const toast = document.getElementById("toast");
+	toast.style.display = "block";
+	setTimeout(() => {
+		toast.style.display = "none";
+	}, 3000);
+}
